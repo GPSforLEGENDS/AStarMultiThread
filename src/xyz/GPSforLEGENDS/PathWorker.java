23e01654fca99731aecf82bc9692b76513239648
Robert Ziegon <robert.ziegon@campus.tu-berlin.de>
2020-04-16 10:46:51 +0200

    removed debugging

--- src/xyz/GPSforLEGENDS/PathWorker.java
+++ src/xyz/GPSforLEGENDS/PathWorker.java
@@ -1,6 +1,5 @@
 package xyz.GPSforLEGENDS;
 
-import java.io.File;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Queue;
@@ -136,7 +135,6 @@ class PathWorker extends Thread{
      */
     private void findBridge(Node currentNode) {
         Node shortest = null;
-        Visualizer.safeNodeGridImageToFile(grid,new File("D:\\RestAPI\\demo\\AStarMultiThread\\tests\\xyz\\GPSforLEGENDS\\resources", "test.png"));
         if(currentNode.getStatus() == 1){
             for(Node neighbour : currentNode.getNeighbours()){
                 if(neighbour.getStatus() == 2){
