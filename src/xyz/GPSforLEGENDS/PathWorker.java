8b8c97fd5209faa836d9086b1cda4ce61324d995
Robert Ziegon <robert.ziegon@campus.tu-berlin.de>
2020-04-20 11:13:00 +0200

    performance improvements

--- src/xyz/GPSforLEGENDS/PathWorker.java
+++ src/xyz/GPSforLEGENDS/PathWorker.java
@@ -13,12 +13,12 @@ class PathWorker extends Thread{
 
     private boolean isStart;
 
-    // Acts as a bridge to find where both finder met
-    private Node from, to;
+    // stores the node where both pathfinder met
+    private Node found;
 
     //sorted by the estimated lowest cost.
     //for more infos look at inner class SortByDistance
-    Queue<Node> openList = new PriorityQueue<>(new SortByDistance());
+    Queue<Node> openList;
 
     //flag to indicate if one pathfinder found the other
     private AtomicBoolean foundFlag;
@@ -39,6 +39,7 @@ class PathWorker extends Thread{
         this.endNode = grid.getNode(endX,endY);
         this.isStart = isStart;
         this.foundFlag = foundFlag;
+        openList = new PriorityQueue<>(new SortByDistance());
     }
 
     /**
@@ -55,6 +56,7 @@ class PathWorker extends Thread{
         this.endNode = end;
         this.isStart = isStart;
         this.foundFlag = foundFlag;
+        openList = new PriorityQueue<>(new SortByDistance());
     }
 
     public void run(){
@@ -65,39 +67,38 @@ class PathWorker extends Thread{
      * function that runs the AStar Pathfinding algorithm.
      * Stops when either the end node is found OR if the pathfinder finds a node that is already closed by the other pathfinder (only if parallel is true in AStar)
      */
-    private void aStarPathfinding(){
+    private void aStarPathfinding() {
         startNode.setCostToReach(0, isStart);
         openList.add(startNode);
 
         do {
             if (!foundFlag.get()) {
                 Node currentNode = openList.poll();
-                synchronized (currentNode) {
-                    if (currentNode.equals(endNode)){
-                        foundFlag.set(true);
-                        return;
+                if (currentNode.equals(endNode)) {
+                    if (foundFlag.compareAndSet(false, true)) {
+                        found = currentNode;
                     }
-                    //check if the other pathfinder already worked on this node
-                    if (currentNode.getStatus() == 1 || currentNode.getStatus() == 2) {
-                        if (foundFlag.compareAndSet(false, true)) {
-                            findBridge(currentNode);
-                        }
-                        return;
-                    }
-
-                    //setting the status
-                    if (isStart) currentNode.setStatus(1);
-                    else currentNode.setStatus(2);
+                    return;
                 }
+
+                if (currentNode.getStatus() == 1 || currentNode.getStatus() == 2) {
+                    if (foundFlag.compareAndSet(false, true)) {
+                        found = currentNode;
+                    }
+                    return;
+                }
+
+                //setting the status
+                if (isStart) currentNode.setStatus(1);
+                else currentNode.setStatus(2);
+
                 //explore neighbours and stop if the pathfinder met each other
                 expandNode(currentNode);
-            }
-            //stop working if the foundFlag is set
-            else{
+            } else {
                 return;
             }
-        } while (!openList.isEmpty()) ;
-
+        } while (!openList.isEmpty());
+        if(!foundFlag.get())
         noPathFound();
     }
 
@@ -107,91 +108,28 @@ class PathWorker extends Thread{
      * @return
      */
     private void expandNode(Node currentNode) {
-        for(Node neighbour : currentNode.getNeighbours()){
-            //stop working as soon as they found each other
-            if(!foundFlag.get()) {
-                synchronized (neighbour) {
-                    if (isStart) {
-                        if (neighbour.getStatus() == 1) continue;
-                    } else {
-                        if (neighbour.getStatus() == 2) continue;
-                    }
-
-                    double costToReach = currentNode.getCostToReach(isStart) + calculateCostToReach(currentNode, neighbour);
-
-                    if (openList.contains(neighbour) && costToReach >= neighbour.getCostToReach(isStart)) continue;
-
-                    neighbour.setCostToReach(costToReach, isStart);
-                    if(neighbour.getStatus() != 1 && neighbour.getStatus() != 2) neighbour.setPredecessor(currentNode);
-
-                    //remove and add since the order might have changed
-                    openList.remove(neighbour);
-                    openList.add(neighbour);
+        for (Node neighbour : currentNode.getNeighbours()) {
+            if (!foundFlag.get()) {
+                if (isStart) {
+                    if (neighbour.getStatus() == 1) continue;
+                } else {
+                    if (neighbour.getStatus() == 2) continue;
                 }
+
+                double costToReach = currentNode.getCostToReach(isStart) + calculateCostToReach(currentNode, neighbour);
+
+                if (openList.contains(neighbour) && costToReach >= neighbour.getCostToReach(isStart)) continue;
+
+                neighbour.setCostToReach(costToReach, isStart);
+                neighbour.setPredecessor(currentNode, isStart);
+
+                //remove and add since the order might have changed
+                openList.remove(neighbour);
+                openList.add(neighbour);
             }
-            //stop working if the foundFlag is set
-            else return;
         }
     }
 
-    /**
-     * finds the bridge from the currentNode to the neighbour node of the opposite status AND the shortest costToReach
-     * @param currentNode
-     */
-    private void findBridge(Node currentNode) {
-        Node shortest = null;
-        if(currentNode.getStatus() == 1){
-            for(Node neighbour : currentNode.getNeighbours()){
-                if(neighbour.getStatus() == 2){
-                    if(shortest == null || shortest.getCostToReach(isStart) > neighbour.getCostToReach(isStart)){
-                        shortest = neighbour;
-                    }
-                }
-            }
-        }
-
-        else if(currentNode.getStatus() == 2){
-            for(Node neighbour : currentNode.getNeighbours()){
-                if(neighbour.getStatus() == 1){
-                    if(shortest == null || shortest.getCostToReach(isStart) > neighbour.getCostToReach(isStart)){
-                        shortest = neighbour;
-                    }
-                }
-            }
-        }
-
-        from = shortest;
-        to = currentNode;
-
-        repairBridge();
-    }
-
-    /**
-     * repairs the bridge in case that the predecessors are wrong because of multithreading
-     */
-    private void repairBridge() {
-        Node shortest = null;
-        for(Node neighbour : from.getNeighbours()){
-            if(neighbour.getStatus() == from.getStatus()){
-                if(shortest == null || shortest.getCostToReach(isStart) > neighbour.getCostToReach(isStart)){
-                    shortest = neighbour;
-                }
-            }
-        }
-        from.setPredecessor(shortest);
-        shortest = null;
-
-        for(Node neighbour : to.getNeighbours()){
-            if(neighbour.getStatus() == to.getStatus()){
-                if(shortest == null || shortest.getCostToReach(!isStart) > neighbour.getCostToReach(!isStart)){
-                    shortest = neighbour;
-                }
-            }
-        }
-        to.setPredecessor(shortest);
-        //TODO remove debugging
-        double x = to.getCostToReach(!isStart);
-    }
 
     /**
      * calculates the cost to reach the neighbour Node from Node n
@@ -210,8 +148,10 @@ class PathWorker extends Thread{
      * function to reset the successor of start and end node to indicate that no path was found
      */
     private void noPathFound(){
-        startNode.setPredecessor(null);
-        endNode.setPredecessor(null);
+        startNode.setPredecessor(null, true);
+        endNode.setPredecessor(null, true);
+        startNode.setPredecessor(null, false);
+        endNode.setPredecessor(null, false);
     }
 
     /**
@@ -230,9 +170,8 @@ class PathWorker extends Thread{
 
             double compared = distanceA - distanceB;
 
-            if(compared < 0) return -1;
             if(compared > 0) return 1;
-            return 0;
+            else return -1;
         }
     }
 
@@ -248,11 +187,7 @@ class PathWorker extends Thread{
         return Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
     }
 
-    Node getBridgeFrom(){
-        return from;
-    }
-
-    Node getBridgeTo(){
-        return to;
+    Node getFound(){
+        return this.found;
     }
 }
